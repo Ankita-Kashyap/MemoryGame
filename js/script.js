1f1867e5d0ef328afcb5d09134baca88d44444d7
Ankita-Kashyap <ankyap150@gmail.com>
2022-03-24 17:14:03 +0530

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
 //Make a list of all memory card elements and store it in the const cards
 const cards = document.querySelectorAll('.memory-card');
 
+let hasflippedCard = false;
+ let firstCard,secondCard;
+
 function flipCard()
 {
     //in this context this keyword refers to the element that fired the event
    // console.log('I was clicked!');
    // console.log(this); 
-    this.classList.toggle('flip');  //classlist ->take all classes toggle->if class is there then remove it if not add it
+    this.classList.add('flip');  //classlist ->take all classes toggle->if class is there then remove it if not add it
 
+    if(!hasflippedCard){
+        //first time player clicked card
+        hasflippedCard=true;
+        firstCard = this;
+       // console.log(hasflippedCard,firstCard);    
+    }
+    else{
+        //second click
+        hasflippedCard = false;
+        secondCard = this;
+
+        // console.log(firstCard,secondCard);
+
+        //check if those cards match
+       
+       // console.log(firstCard.dataset.framework); console.log(secondCard.dataset.framework); Displays names
+    }
 
 
 }
