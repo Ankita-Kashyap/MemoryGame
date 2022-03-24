8b74ab852ab261a4f423e133c01e270f98d56e41
Ankita-Kashyap <ankyap150@gmail.com>
2022-03-24 18:16:12 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,65 +1,66 @@
 //Make a list of all memory card elements and store it in the const cards
 const cards = document.querySelectorAll('.memory-card');
 
-let hasflippedCard = false;
-let lockBoard = false;    // to lock the board : so that the cards first unflip then the user takes another try
- let firstCard,secondCard;
+let hasFlippedCard = false;
+//corner case 1 ->  // to lock the board : so that the cards first unflip then the user takes another try
+//corner case 2 ->double click we get same dataframework so they match and give us wrong ans
+let lockBoard = false;
+let firstCard, secondCard;
 
-function flipCard()
-{
-    if(lockBoard) return; 
-    //in this context this keyword refers to the element that fired the event
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  //in this context this keyword refers to the element that fired the event
    // console.log('I was clicked!');
    // console.log(this); 
-    this.classList.add('flip');  //classlist ->take all classes toggle->if class is there then remove it if not add it
 
-    if(!hasflippedCard){
-        //first time player clicked card
-        hasflippedCard=true;
-        firstCard = this;
+  this.classList.add('flip');
 
-       // console.log(hasflippedCard,firstCard);    
-    }
-    else{
-        //second click
-        hasflippedCard = false;
-        secondCard = this;
+  if (!hasFlippedCard) {
+    //first time player clicked card
+    hasFlippedCard = true;
+    firstCard = this;
 
-        // console.log(firstCard,secondCard);
-       checkForMatch();
-      
-        }
+    return;
+     // console.log(hasflippedCard,firstCard);    
+  }
 
-
-        function checkForMatch()
-        {    // console.log(firstCard.dataset.framework); console.log(secondCard.dataset.framework); Displays names
-            //check if those cards match
-            let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-            isMatch? disableCards() : unflipCards();
-           
-        }
-        function disableCards()
-        {
-            //its a match
-            firstCard.removeEventListener('click',flipCard);
-            secondCard.removeEventListener('click',flipCard);
-        }
-
-        function unflipCards()
-        {
-            lockBoard = true;
-             //not a match
-             setTimeout(() =>{
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-                lockBoard = false;
-                },1000);
-        }
-
+  // second click
+  secondCard = this;
+ // console.log(firstCard,secondCard);
+  checkForMatch();
 }
 
+function checkForMatch() {
+    // console.log(firstCard.dataset.framework); console.log(secondCard.dataset.framework); Displays names
+            //check if those cards match
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
-cards.forEach(card => card.addEventListener('click',flipCard));
+  isMatch ? disableCards() : unflipCards();
+}
 
+function disableCards() {
+    //its a match
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
 
-//15:48
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+//not a match
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1000);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
